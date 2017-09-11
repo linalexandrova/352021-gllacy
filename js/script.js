e00f320a0e0d31c38123af7493ed41b4c6a5a86e
linalexandrova <linalexandrova@gmail.com>
2017-09-11 23:23:17 +0300

    1109

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@ var feedbackButton = document.getElementById("feedbackButton");
 var feedbackForm = document.getElementById("feedbackForm");
 var overlayBlack = document.getElementById("overlayBlack");
 var crossForm = document.getElementById("crossForm");
-feedbackButton.addEventListener( "click" , function() {
-  if(feedbackForm.classList.contains("hidden")){
+feedbackButton.addEventListener("click", function () {
+  if (feedbackForm.classList.contains("hidden")) {
     feedbackForm.classList.remove("hidden");
     overlayBlack.classList.remove("hidden");
-  }else{
+  } else {
     feedbackForm.classList.add('hidden');
   }
 });
-crossForm.addEventListener("click" , function() {
+crossForm.addEventListener("click", function () {
   feedbackForm.classList.add('hidden');
   overlayBlack.classList.add('hidden');
 });
